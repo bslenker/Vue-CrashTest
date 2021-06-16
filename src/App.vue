8c5606f1cee571b66c851a8f66e48bb90c240a9f
Ben Slenker <75644117+bslenker@users.noreply.github.com>
2021-06-16 14:50:59 -0500

    btn toggle on click added for task addition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
 <div class="container">
-  <Header title="Task Tracker"/>
+  <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask"/>
+  <div v-show="showAddTask">
   <AddTask  @add-task="addTask" />
+  </div>
   <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
 </div>
 
@@ -21,10 +23,15 @@ export default {
   },
   data() {
     return {
-      tasks: []
+      tasks: [],
+      showAddTask: false,
     }
   },
   methods: {
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    },
+
     addTask(task) {
       this.tasks = [...this.tasks, task]
     },
